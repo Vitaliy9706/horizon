5389602c878ba78a24abe775778a72dd08a3cc51
Vitaliy Oleinik <74061826+Vitaliy9706@users.noreply.github.com>
2024-02-21 00:45:09 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,113 @@
+const app = require("express")();
+const axios = require("axios");
+
+let chrome = {};
+let puppeteer;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  puppeteer = require("puppeteer");
+}
+
+app.get("/abudhabi", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://hub.tissl.com/login?returnUrl=%2Fhome");
+    await page.waitForSelector('input[formcontrolname="username"]');
+
+    await page.type('input[formcontrolname="username"]', "Vitaly");
+    await page.type('input[formcontrolname="password"]', "123");
+    await page.click("button.btn.btn-primary");
+
+    await page.waitForNavigation();
+
+    await page.goto(
+      "https://hub.tissl.com/report-centre/sales-percentage-vs-total"
+    );
+    const token = await page.evaluate(() => {
+      return localStorage.getItem("token");
+    });
+    // Send the token to the external API
+    const apiEndpoint = "https://api.vitaliy.digital/new-token.php";
+    const apiResponse = await axios.post(apiEndpoint, { token });
+
+    // Check the API response and handle it accordingly
+    console.log("API Response:", apiResponse.data);
+
+    // Send a response to the client
+    res.send(apiResponse.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Dubai endpoint
+app.get("/dubai", async (req, res) => {
+  let options = {};
+
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  try {
+    let browser = await puppeteer.launch(options);
+
+    let page = await browser.newPage();
+    await page.goto("https://hub.tissl.com/login?returnUrl=%2Fhome");
+    await page.waitForSelector('input[formcontrolname="username"]');
+
+    await page.type('input[formcontrolname="username"]', "harish1");
+    await page.type('input[formcontrolname="password"]', "dubai");
+    await page.click("button.btn.btn-primary");
+
+    await page.waitForNavigation();
+
+    await page.goto(
+      "https://hub.tissl.com/report-centre/sales-percentage-vs-total"
+    );
+    const token = await page.evaluate(() => {
+      return localStorage.getItem("token");
+    });
+    // Send the token to the external API
+    const apiEndpoint = "https://api.vitaliy.digital/dubai/new-token.php";
+    const apiResponse = await axios.post(apiEndpoint, { token });
+
+    // Check the API response and handle it accordingly
+    console.log("API Response:", apiResponse.data);
+
+    // Send a response to the client
+    res.send(apiResponse.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
+
+module.exports = app;
